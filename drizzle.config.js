869862d3b775d9aae5df7f0faed27493dd47dfbb
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-05-16 20:20:32 +0530

    feat: update database schema and configuration, remove unused logs and SQL files

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,9 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 import { config } from 'dotenv';
-config({ path: '.env.local' });
+config({ path: '.env' });
+
+
 export default defineConfig({
   schema: './src/db/schema.js',
   out: './drizzle',
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,6 @@
+import { redirect } from 'next/navigation';
+
 export default function HomePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center px-6 bg-slate-50">
-      <div className="rounded-lg border border-slate-200 bg-white px-8 py-6 text-center text-lg font-semibold text-slate-800 shadow-sm">
-        Public homepage
-      </div>
-    </div>
-  );
+  redirect('https://geologymining.jk.gov.in/');
 }
+
